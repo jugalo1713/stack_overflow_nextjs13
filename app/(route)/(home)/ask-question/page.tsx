a1ee11548168132c6ffce94571415ff73abaa913
julian-gallo-nexer <julian.gallo2@nexergroup.com>
2023-12-20 23:36:09 -0500

    initial form and editor created

--- app/(route)/(home)/ask-question/page.tsx
+++ app/(route)/(home)/ask-question/page.tsx
@@ -1,7 +1,15 @@
+import Question from "@/components/Forms/Question";
 import React from "react";
 
 const AskQuestions = () => {
-  return <div>Ask Questions</div>;
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+      <div className="my-9">
+        <Question />
+      </div>
+    </div>
+  );
 };
 
 export default AskQuestions;
