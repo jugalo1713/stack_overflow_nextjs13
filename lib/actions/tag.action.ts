886530fe1396f289f16f2b363dce00a0ea08b10b
julian-gallo-nexer <julian.gallo2@nexergroup.com>
2024-01-10 16:59:16 -0500

    tags page created

--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -1,8 +1,9 @@
 "use server";
 
 import { connectToDatabase } from "../mongoose";
-import { GetTopInteractedTagsParams } from "./shared.types";
+import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared.types";
 import User from "../database/user.model";
+import Tag from "../database/tag.model";
 
 // export async function getAllUsers(params: GetAllUsersParams) {
 //   try {
@@ -32,6 +33,18 @@ export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
       { _id: "2", name: "tag2" },
       { _id: "3", name: "tag3" },
     ];
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+export async function getAllTags(params: GetAllTagsParams) {
+  try {
+    connectToDatabase();
+    const tags = await Tag.find({});
+
+    return { tags };
   } catch (error) {
     console.error(error);
     throw error;
