fe9d3c713500abf5e4cb71f14875bdae8b141804
julian-gallo-nexer <julian.gallo2@nexergroup.com>
2024-01-15 15:52:05 -0500

    voting finished

---
+++ app/(route)/collection/page.tsx
@@ -0,0 +1,59 @@
+import QuestionsCard from "@/components/cards/QuestionsCard";
+import NoResult from "@/components/shared/NoResult";
+import Filter from "@/components/shared/search/Filter";
+import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
+import { QuestionFilters } from "@/constants/filters";
+import { getSavedQuestions } from "@/lib/actions/user.action";
+import { auth } from "@clerk/nextjs";
+
+export default async function Home() {
+  const { userId } = auth();
+  if (!userId) return null;
+
+  const result = await getSavedQuestions({ clerkId: userId });
+
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">Saved Questions</h1>
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearchbar
+          route="/"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for questions"
+          otherClasses="flex-1"
+        />
+        <Filter
+          filters={QuestionFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+        />
+      </div>
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {result.questions.length > 0 ? (
+          result.questions.map((item) => (
+            <QuestionsCard
+              key={item._id}
+              _id={item._id}
+              title={item.title}
+              tags={item.tags}
+              author={item.author}
+              upvotes={item.upvotes}
+              views={item.views}
+              answers={item.answers}
+              createdAt={item.createdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="There's no saved questions to show"
+            description="Be the first to break the silence! Ask a Question and kickstart the
+        discussion. Our query could be the next big thing others learn from. Get
+        involved!"
+            linkTitle="Ask a question"
+            link="/ask-question"
+          />
+        )}
+      </div>
+    </>
+  );
+}
